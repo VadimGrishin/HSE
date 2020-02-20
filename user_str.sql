cf7df863a04590689545c375808c2071bfb1f6bd
VadimGrishin <37953929+VadimGrishin@users.noreply.github.com>
2020-02-20 17:38:29 +0300

    Update user_str.sql

--- user_str.sql
+++ user_str.sql
@@ -34,30 +34,39 @@
 
 join
 
-  ( 
+  (
     select  
-      item_id, assessment_ext_id, hse_user_ext_id, (action_ts - action_start_ts) time_dlt 
-      , sum(response_score)  resp_sum
-      , rank() OVER (PARTITION BY item_id ORDER BY sum(response_score), (action_ts - action_start_ts) desc, hse_user_ext_id ) rnk 
-      , cume_dist() OVER (PARTITION BY item_id ORDER BY sum(response_score), (action_ts - action_start_ts) desc, hse_user_ext_id ) cd
-    from caq_event 
-    join
-       ( 
-        select course_id, hse_user_ext_id, ia.item_id, assessment_ext_id, action_version, min(action_start_ts) action_start_ts
-         from caq_event
-          join coursera_structure.assessment a on a.ext_id=assessment_ext_id and a.load_id=81 --!!!!
-          join coursera_structure.assessment_type at on at.id=a.type_id
-          join coursera_structure.item_assessment ia on ia.assessment_id=a.id
-        where course_id=123 and question_ext_id is not null and a.type_id=7 --7=summative
-        group by course_id, hse_user_ext_id, ia.item_id, assessment_ext_id, action_version
-       )   as x using(course_id, hse_user_ext_id, assessment_ext_id, action_start_ts, action_version)
-    group by item_id, assessment_ext_id, hse_user_ext_id, (action_ts - action_start_ts)
+          item_id, assessment_ext_id, hse_user_ext_id, time_dlt 
+          , sum(response_score)  resp_sum
+          , sum(question_var) si2
+          , count(*) k
+          , variance(sum(response_score)) over (partition by assessment_ext_id) sx2
+          , (1 - sum(question_var) / (variance(sum(response_score)) over (partition by assessment_ext_id) + 0.01)/ (1.01 - 1/count(*))) reliab
+          , rank() OVER (PARTITION BY item_id ORDER BY sum(response_score), time_dlt desc, hse_user_ext_id ) rnk 
+          , cume_dist() OVER (PARTITION BY item_id ORDER BY sum(response_score), time_dlt desc, hse_user_ext_id ) cd
+      from
+      ( 
+        select  
+          item_id, assessment_ext_id, question_ext_id, response_score, hse_user_ext_id, (action_ts - action_start_ts) time_dlt 
+          , variance(response_score) over(partition by assessment_ext_id, question_ext_id) question_var 
+          
+        from caq_event 
+        join
+           ( 
+            select course_id, hse_user_ext_id, ia.item_id, assessment_ext_id, min(action_start_ts) action_start_ts 
+             from caq_event
+              join coursera_structure.assessment a on a.ext_id=assessment_ext_id and a.load_id=81 --!!!!
+              join coursera_structure.assessment_type at on at.id=a.type_id
+              join coursera_structure.item_assessment ia on ia.assessment_id=a.id
+            where course_id=123 and question_ext_id is not null and a.type_id=7 --7=summative
+            group by course_id, hse_user_ext_id, ia.item_id, assessment_ext_id --, action_version
+           )   as x using(course_id, hse_user_ext_id, assessment_ext_id, action_start_ts) --, action_version
+       where question_ext_id is not null  -- рабочая гипотеза: есть акции, которые старутют одновременно, но среди них только одна, которая с вопросами-ответами
+      ) as xx
+      group by item_id, assessment_ext_id, hse_user_ext_id, time_dlt
   ) as user_range
 
 using(item_id, assessment_ext_id)
-
+--where hse_user_ext_id='0cce6902d6b689b2ac40c2ef39394bdf5afe4d1a'
 order by  cname, bid, m_ord, l_ord, i_ord, aq_ord, user_range.cd
   --limit 1000
-
-
-
